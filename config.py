9446763523983c8cd138340854dd6397940f953f
Blackmamba <122316561+FantasticSukhi@users.noreply.github.com>
2025-07-31 19:57:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,32 @@
 # Bot Configuration
 BOT_TOKENS = [
     "7747234167:AAGsuZlyXZwIarywwFyKcHPFVEYfBY7xcpk",
-    "8161769845:AAF2QYkJFcKTreGJF7keex64uVdapVU5muc"
+    "8161769845:AAF2QYkJFcKTreGJF7keex64uVdapVU5muc",
+    "8476810007:AAHiJ2FRCAq_2Hqx398xTXmS1xP5G4e6vRk",
+    "8375610509:AAHyMpVzjk0WWx7umbNdzZSraf8Za3rzoL4",
+    "7595546571:AAGLG9aDkabrdqQDqL_AKBscNCQxXkgVFBY",
+    "7790249980:AAGNQ7mHuqWR9KuZE_VXQz1CXfwXaQNouwk",
+    "7948107635:AAEcyf5Wb-KXVqUn6MeSUBGkFx9qXM7ZfQA",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    ""
+    
+    
 ]
 
 # Support links
